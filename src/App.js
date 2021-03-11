56316464e5158b0cc917be6790ca5cdafb9b3133
Przemysław Chudzia <prz.chud@gmail.com>
2021-03-11 16:09:56 +0100

    Change app component to functional component and implement useEffect hook which sets component state with user list data

--- src/App.js
+++ src/App.js
@@ -1,15 +1,22 @@
-import React, { Component } from 'react';
+import React, {useEffect, useState} from 'react';
 import DataTable from './DataTable';
 import './App.css';
+import {userListMock} from './mocks/userListMock';
 
-class App extends Component {
-  render() {
+
+const App = () => {
+
+    const [userData, setUserData] = useState([]);
+
+    useEffect(() => {
+        setUserData(userListMock)
+    }, [])
+
     return (
-      <div className="container mt-3">
-        <DataTable rows={this.props.rows} locale="da" rowsPerPage={5} />
-      </div>
+        <div className="container mt-3">
+            <DataTable rows={userData} rowsPerPage={5}/>
+        </div>
     );
-  }
 }
 
 export default App;
