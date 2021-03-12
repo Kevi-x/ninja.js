518a42bc867e2d252606e51769e5c8b9cb3302de
Przemysław Chudzia <prz.chud@gmail.com>
2021-03-12 10:43:48 +0100

    Add default params, change the way page items are created

--- src/DataTable/Pagination.jsx
+++ src/DataTable/Pagination.jsx
@@ -2,26 +2,28 @@ import React from 'react'
 
 import Page from './Page'
 
-const Pagination = ({ currentPageNumber, totalNumberOfPages, onChange }) => {
-  const pages =
-    Array
-      .from(Array(totalNumberOfPages).keys())
-      .map(pageNumber => {
-        return <Page
-          key={pageNumber}
-          currentPageNumber={currentPageNumber}
-          pageNumber={pageNumber}
-          onChange={onChange} />
-      })
+const Pagination = ({currentPageNumber = 0, totalNumberOfPages, onChange}) => {
 
-  if (pages.length <= 1) {
-    return null
-  }
-  return(
-    <ul className="pagination">
-      {pages}
-    </ul>
-  )
+    if (totalNumberOfPages <= 1) {
+        return null;
+    }
+
+    const pages =
+        [...Array(totalNumberOfPages).keys()]
+            .map(pageNumber => {
+                return <Page
+                    key={pageNumber}
+                    currentPageNumber={currentPageNumber}
+                    pageNumber={pageNumber}
+                    onChange={onChange}/>
+            })
+
+
+    return (
+        <ul className="pagination">
+            {pages}
+        </ul>
+    )
 }
 
 export default Pagination
