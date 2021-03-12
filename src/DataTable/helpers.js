1e7a36d2c6b8a93895df44af90b16d1c2ffe638c
Przemysław Chudzia <prz.chud@gmail.com>
2021-03-12 09:41:15 +0100

    Add additional checks for helper functions and add comments

--- src/DataTable/helpers.js
+++ src/DataTable/helpers.js
@@ -1,18 +1,61 @@
+import {config} from '../config/config';
+
+/**
+ * Calculates total number of available pages
+ * @param total total count of data rows
+ * @param rowsPerPage number of rows per page
+ * @return {number} number of pages
+ */
 export const calculateTotalNumberOfPages = (total, rowsPerPage) => {
 
-    if (rowsPerPage === 0) {
-        return 0
+    if (!isNumber(total) || !isNumber(rowsPerPage)) {
+        return 0;
     }
+
+
     return Math.ceil(total / rowsPerPage)
 }
 
+/**
+ * Gets a start index and an end index for given page number and rows per page. When either of parameters is not a
+ * number or rowsPerPage is 0, it returns default values got from the config
+ * @param pageNumber current page number
+ * @param rowsPerPage number of rows per page
+ * @return {[number, *]|[number, number]} [startIndex, endIndex]
+ */
 export const getDataStartEndIndexOnPage = (pageNumber, rowsPerPage) => {
+    if (!isNumber(pageNumber) || !isNumber(rowsPerPage) || rowsPerPage === 0) {
+        return [0, config.defaultRowsPerPage];
+    }
+
     const startIndex = pageNumber * rowsPerPage
     return [startIndex, startIndex + rowsPerPage]
 }
 
+
+/**
+ * Gets data for current page
+ * @param data displayed data
+ * @param currentPageNumber current page number
+ * @param rowsPerPage rows per page
+ * @return {*} data to be displayed
+ */
 export const getPageData = (data, currentPageNumber, rowsPerPage) => {
     const [startIndex, endIndex] = getDataStartEndIndexOnPage(currentPageNumber, rowsPerPage);
 
     return data.slice(startIndex, endIndex);
 }
+
+/**
+ * Checks if value is null or undefined
+ * @param value value to be checked
+ * @return {boolean}
+ */
+export const isNullOrUndefined = (value) => value === null || value === undefined;
+
+/**
+ * Checks if value is a number and is finite
+ * @param value value to be checked
+ * @return {boolean}
+ */
+export const isNumber = (value) => typeof value === 'number' && isFinite(value);
